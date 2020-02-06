cf30a8d818a69d6bb663db83150cdbacea0e6dd6
Santosh Vaza <11908578+svaza@users.noreply.github.com>
2020-02-06 10:29:10 +0530

    dummy - rename app.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import App from './App';
+import App from './app1';
 import './sass/main.scss';
 
 ReactDOM.render(<App input1='ss'></App>, document.getElementById('app-root'));
