9d217f87a78a5312bdd451266bae0071fb0d3b69
Santosh Vaza <vazasantosh@gmail.com>
2020-02-06 09:23:02 +0000

    add index.html and site

---
+++ site/precache-manifest.866cea68c7adb9345c4b41fc9730e203.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7668e8e39264b7970c7661502cf325e5",
+    "url": "/svaza/weather-app/index.html"
+  },
+  {
+    "revision": "1f1355926bbfcacd0096",
+    "url": "/svaza/weather-app/static/css/main.6a36e7ae.chunk.css"
+  },
+  {
+    "revision": "d7327d1ab4af69a583fb",
+    "url": "/svaza/weather-app/static/js/2.10d26810.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/svaza/weather-app/static/js/2.10d26810.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f1355926bbfcacd0096",
+    "url": "/svaza/weather-app/static/js/main.57968ceb.chunk.js"
+  },
+  {
+    "revision": "89410e8ff37b42968efc",
+    "url": "/svaza/weather-app/static/js/runtime-main.9bc3e69f.js"
+  }
+]);
